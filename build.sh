23421c07ba45bb96ff2e60aa94b8b763470afc22
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-13 20:10:21 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Django dependencies..."
+pip install -r requirements.txt
+
+echo "Building React app..."
+cd UI/supermind-ui
+npm install
+npm run build
+cd ..
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Django server..."
+python manage.py runserver

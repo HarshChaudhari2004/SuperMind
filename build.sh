74cde21955961bf02b506ed7f65b787683e2b533
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-26 21:43:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 cd SuperMind
-pwd
+echo "current directory is: $(pwd)"
+echo "list of files in current directory is: $(ls)"
 
 echo "Installing Django dependencies..."
 pip install -r requirements.txt
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
 
 echo "Starting Django server..."

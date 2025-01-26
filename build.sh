bc25ec9e333fb6a363d0285d2d8c4793259db72f
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-26 21:28:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Installing Django dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python SuperMind/manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "Starting Django server..."
 cd SuperMind

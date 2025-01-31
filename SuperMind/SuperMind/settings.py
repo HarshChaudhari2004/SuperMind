51712852eaca8560d1166482b0a9739902047d7e
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-31 19:32:08 +0530

    Update settings.py

--- SuperMind/SuperMind/settings.py
+++ SuperMind/SuperMind/settings.py
@@ -35,6 +35,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = ['supermind-production.up.railway.app', 'localhost', '127.0.0.1', 'supermind-9fii.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
